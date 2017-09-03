8e8a6a66e20059c31bc5defc102068d3e86fa530
hkoichi <hkoichi925@gmail.com>
2017-09-03 23:23:40 +0900

    アクティビティにdataBindingの適用

--- app/src/main/java/com/app/koichihasegawa/githubsearchapp/presentation/search/SearchActivity.java
+++ app/src/main/java/com/app/koichihasegawa/githubsearchapp/presentation/search/SearchActivity.java
@@ -1,9 +1,11 @@
 package com.app.koichihasegawa.githubsearchapp.presentation.search;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.app.koichihasegawa.githubsearchapp.R;
+import com.app.koichihasegawa.githubsearchapp.databinding.ActivitySearchBinding;
 import com.app.koichihasegawa.githubsearchapp.di.search.SearchModule;
 import com.app.koichihasegawa.githubsearchapp.presentation.GithubSearchApp;
 
@@ -16,14 +18,15 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        // DataBinding
+        ActivitySearchBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
 
         // DI
         ((GithubSearchApp) this.getApplication()).component
                 .addSearchComponent(new SearchModule())
                 .inject(this);
 
-        // TODO: 検索機能と、レイアウトの追加
+        searchPresenter.setUpRecyclerView(binding.recyclerView);
     }
 
 }
